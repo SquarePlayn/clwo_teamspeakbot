c6c19bb380a59368b719d82b89bd55778ac3bef6
SquarePlayn <pietervoors@gmail.com>
2019-02-16 17:14:55 +0100

    Only @ the dev on Slack on the most important messages

---
+++ modules/channel_activity.py
@@ -0,0 +1,24 @@
+"""
+Logs the activities of channels per hour
+"""
+from client import Client
+from settings import Settings
+import time
+
+Settings.load_settings({"channel_activity"})
+settings = Settings.settings
+
+
+def execute(ts, db):
+    table = settings["channel_activity"]["db_table_name"]
+    cid_column = settings["channel_activity"]["db_cid_column_name"]
+    hour_column = settings["channel_activity"]["db_hour_column_name"]
+    points_column = settings["channel_activity"]["db_points_column_name"]
+
+    current_time = int(time.time())
+    hour = current_time / 60 / 60
+
+    for client in Client.clients.values():
+        cid = client.cid
+        db.execute("INSERT INTO "+table+"("+cid_column+", "+hour_column+", "+points_column+") VALUES (" +
+                   str(cid)+", "+str(hour)+", 1) ON DUPLICATE KEY UPDATE "+points_column+" = "+points_column+" + 1;")
