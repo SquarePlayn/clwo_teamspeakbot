983feb4871b905c0a1a1bbc4e0b093ae3db4183a
SquarePlayn <pietervoors@gmail.com>
2019-02-17 21:04:26 +0100

    Channel activity provides function to Channel class to get activity

--- modules/channel_activity.py
+++ modules/channel_activity.py
@@ -1,20 +1,48 @@
 """
 Logs the activities of channels per hour
+Also adds a function to the channel class that lets you get the activity of a channel
 """
+import decimal
+import time
+from channel import Channel
 from client import Client
 from settings import Settings
-import time
 
 Settings.load_settings({"channel_activity"})
 settings = Settings.settings
 
+table = settings["channel_activity"]["db_table_name"]
+cid_column = settings["channel_activity"]["db_cid_column_name"]
+hour_column = settings["channel_activity"]["db_hour_column_name"]
+points_column = settings["channel_activity"]["db_points_column_name"]
+
+
+def init(ts, db):
+
+    # Returns the activity logged in a channel from min_hour (incl) and stores it in a dict activity (key = min_hour)
+    def get_activity(self, min_hour):
+        if not hasattr(self, "activity"):
+            self.activity = dict()
+        if min_hour not in self.activity:
+            cid = self.cid
+            query = "SELECT SUM("+points_column+") AS activity FROM "+table+" WHERE "+cid_column+" = "+str(cid) + \
+                          " AND "+hour_column+" >= "+str(min_hour)+";"
+            db.execute(query)
+
+            # Returns 1 row containing the activity score (as a decimal.Decimal), or None if no score found
+            db_activity = db.fetchone()["activity"]
+            if isinstance(db_activity, (int, decimal.Decimal)):
+                self.activity[min_hour] = int(db_activity)
+            else:
+                self.activity[min_hour] = 0
+
+        return self.activity[min_hour]
+
+    # Add the above method to the Client class
+    setattr(Channel, "get_activity", get_activity)
+
 
 def execute(ts, db):
-    table = settings["channel_activity"]["db_table_name"]
-    cid_column = settings["channel_activity"]["db_cid_column_name"]
-    hour_column = settings["channel_activity"]["db_hour_column_name"]
-    points_column = settings["channel_activity"]["db_points_column_name"]
-
     current_time = int(time.time())
     hour = int(current_time / 60 / 60)
 
